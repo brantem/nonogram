a730c302ce850408dbdefa345133d66ea3d0d929
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2025-02-17 17:49:26 +0700

    Tidy up

--- src/components/GenerateFromImageModal/index.tsx
+++ src/components/GenerateFromImageModal/index.tsx
@@ -38,11 +38,11 @@ const generateDefaultData = (): Data => ({
 });
 
 export default function GenerateFromImageModal({ ref }: { ref: React.Ref<GenerateFromImageModalHandle> }) {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
   const [isOpen, setIsOpen] = useState(false);
+
   const [file, setFile] = useState<FileWithPreview | null>(null);
   const [data, setData] = useState<Data>(generateDefaultData);
+
   const [isGenerating, setIsGenerating] = useState(false);
   const [grid, setGrid] = useState<types.Cell[][]>([]);
   const debouncedIsGenerating = useDebounce(isGenerating, 150);
@@ -70,7 +70,6 @@ export default function GenerateFromImageModal({ ref }: { ref: React.Ref<Generat
       const { status, ...message } = e.data;
       switch (status) {
         case 'success':
-          preview(message.data);
           setGrid(message.data);
           break;
         default:
@@ -86,73 +85,6 @@ export default function GenerateFromImageModal({ ref }: { ref: React.Ref<Generat
     };
   }, [file, debouncedData]);
 
-  const preview = (grid: types.Cell[][]) => {
-    const canvas = canvasRef.current!;
-    const ctx = canvas.getContext('2d')!;
-
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-
-    const rows = grid.length;
-    const cols = grid[0].length;
-    const cellSize = canvas.height / Math.max(rows, cols);
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    // draw filled
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        if (grid[i][j][0] === 1) {
-          const x = j * cellSize;
-          const y = i * cellSize;
-          ctx.fillStyle = 'black';
-          ctx.fillRect(x, y, cellSize, cellSize);
-        }
-      }
-    }
-
-    // draw the 5x5 groups
-    for (let groupRow = 0; groupRow < Math.ceil(rows / 5); groupRow++) {
-      for (let groupCol = 0; groupCol < Math.ceil(cols / 5); groupCol++) {
-        const groupX = groupCol * 5 * cellSize;
-        const groupY = groupRow * 5 * cellSize;
-        const groupWidth = Math.min(5, cols - groupCol * 5) * cellSize;
-        const groupHeight = Math.min(5, rows - groupRow * 5) * cellSize;
-
-        // group border
-        ctx.strokeStyle = '#737373';
-        ctx.lineWidth = 2;
-        ctx.strokeRect(groupX, groupY, groupWidth, groupHeight);
-
-        for (let i = 0; i < 5; i++) {
-          for (let j = 0; j < 5; j++) {
-            const cellRow = groupRow * 5 + i;
-            const cellCol = groupCol * 5 + j;
-
-            if (cellRow < rows && cellCol < cols) {
-              const x = cellCol * cellSize;
-              const y = cellRow * cellSize;
-
-              // cell border
-              ctx.strokeStyle = '#737373';
-              ctx.lineWidth = 1;
-              ctx.strokeRect(x, y, cellSize, cellSize);
-            }
-          }
-        }
-      }
-    }
-
-    // outer border
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 2;
-
-    // adjust the coordinates to account for the 2px border
-    ctx.strokeRect(1, 1, cols * cellSize - 2, rows * cellSize - 2);
-  };
-
   const closeAndReset = () => {
     setIsOpen(false);
 
@@ -161,6 +93,7 @@ export default function GenerateFromImageModal({ ref }: { ref: React.Ref<Generat
     setFile(null);
 
     setData(generateDefaultData);
+    setGrid([]);
     setIsGenerating(false);
   };
 
@@ -232,72 +165,11 @@ export default function GenerateFromImageModal({ ref }: { ref: React.Ref<Generat
             </div>
           </div>
 
-          <div
-            className={cn(
-              'sticky top-0 grid divide-neutral-200 bg-neutral-50 max-md:divide-y md:grid-cols-4 md:divide-x',
-              'dark:divide-neutral-800 dark:bg-neutral-950',
-            )}
-          >
-            <div className="flex items-center justify-between gap-2 p-2 pl-3 text-sm text-neutral-500">
-              <span>Brightness</span>
-              <input
-                className="flex-1 disabled:opacity-50 max-md:max-w-1/2"
-                type="range"
-                min="0"
-                max="255"
-                step="1"
-                value={data.options.threshold}
-                onChange={(e) => {
-                  setData((prev) => ({ ...prev, options: { ...prev.options, threshold: parseInt(e.target.value) } }));
-                }}
-                disabled={!file || debouncedIsGenerating}
-              />
-            </div>
-            <div className="flex items-center justify-between gap-2 pl-3 text-sm text-neutral-500">
-              <span className="py-2">Image Fit</span>
-              <select
-                className="h-full border-0 bg-transparent py-0 text-right text-sm focus:outline-none disabled:opacity-50"
-                value={data.options.fit}
-                onChange={(e) => {
-                  setData((prev) => ({ ...prev, options: { ...prev.options, fit: e.target.value as Options['fit'] } }));
-                }}
-                disabled={!file || debouncedIsGenerating}
-              >
-                <option value="cover">Cover</option>
-                <option value="contain">Contain</option>
-              </select>
-            </div>
-            <label className="flex items-center justify-between gap-2 px-3 py-2 text-sm text-neutral-500">
-              <span>Invert</span>
-              <input
-                type="checkbox"
-                className="rounded disabled:opacity-50"
-                checked={data.options.invert}
-                onChange={(e) => {
-                  setData((prev) => ({ ...prev, options: { ...prev.options, invert: e.target.checked } }));
-                }}
-                disabled={!file || debouncedIsGenerating}
-              />
-            </label>
-            <div className="flex items-center justify-between gap-2 pl-3 text-sm text-neutral-500">
-              <span className="py-2">Smoothness</span>
-              <select
-                className="h-full rounded-tr-md border-0 bg-transparent py-0 text-right text-sm disabled:opacity-50"
-                value={data.options.smoothness}
-                onChange={(e) => {
-                  setData((prev) => ({
-                    ...prev,
-                    options: { ...prev.options, smoothness: parseInt(e.target.value) as Options['smoothness'] },
-                  }));
-                }}
-                disabled={!file || debouncedIsGenerating}
-              >
-                <option value="0">None</option>
-                <option value="1">Low</option>
-                <option value="2">High</option>
-              </select>
-            </div>
-          </div>
+          <Options
+            value={data.options}
+            onChange={(options) => setData((prev) => ({ ...prev, options }))}
+            isDisabled={!file || debouncedIsGenerating}
+          />
 
           <div
             className={cn(
@@ -319,6 +191,7 @@ export default function GenerateFromImageModal({ ref }: { ref: React.Ref<Generat
                     return Object.assign(file, { preview: URL.createObjectURL(file) });
                   });
                 }}
+                disabled={isGenerating || debouncedIsGenerating}
               />
               {file ? (
                 <img src={file.preview} className="size-full object-cover" />
@@ -327,25 +200,168 @@ export default function GenerateFromImageModal({ ref }: { ref: React.Ref<Generat
               )}
             </label>
 
-            <div className="relative aspect-square">
-              {debouncedIsGenerating && (
-                <div
-                  className={cn(
-                    'absolute top-2 left-1/2 -translate-x-1/2 rounded-md border border-neutral-200 bg-white px-3 py-1 text-sm',
-                    'dark:border-neutral-800',
-                  )}
-                >
-                  Generating...
-                </div>
-              )}
-              <div className="absolute inset-0 flex items-center justify-center text-sm text-neutral-500">
-                The generated grid will be shown here
-              </div>
-              <canvas ref={canvasRef} className="relative size-full dark:invert" />
-            </div>
+            <Preview grid={grid} isGenerating={debouncedIsGenerating} />
           </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
+  );
+}
+
+type OptionsProps = {
+  value: Options;
+  onChange(options: Options): void;
+  isDisabled: boolean;
+};
+
+function Options({ value, onChange, isDisabled }: OptionsProps) {
+  return (
+    <div
+      className={cn(
+        'sticky top-0 grid divide-neutral-200 bg-neutral-50 max-md:divide-y md:grid-cols-4 md:divide-x',
+        'dark:divide-neutral-800 dark:bg-neutral-950',
+      )}
+    >
+      <div className="flex items-center justify-between gap-2 p-2 pl-3 text-sm text-neutral-500">
+        <span>Brightness</span>
+        <input
+          className="flex-1 disabled:opacity-50 max-md:max-w-1/2"
+          type="range"
+          min="0"
+          max="255"
+          step="1"
+          value={value.threshold}
+          onChange={(e) => onChange({ ...value, threshold: parseInt(e.target.value) })}
+          disabled={isDisabled}
+        />
+      </div>
+      <div className="flex items-center justify-between gap-2 pl-3 text-sm text-neutral-500">
+        <span className="py-2">Image Fit</span>
+        <select
+          className="h-full border-0 bg-transparent py-0 text-right text-sm focus:outline-none disabled:opacity-50"
+          value={value.fit}
+          onChange={(e) => onChange({ ...value, fit: e.target.value as Options['fit'] })}
+          disabled={isDisabled}
+        >
+          <option value="cover">Cover</option>
+          <option value="contain">Contain</option>
+        </select>
+      </div>
+      <label className="flex items-center justify-between gap-2 px-3 py-2 text-sm text-neutral-500">
+        <span>Invert</span>
+        <input
+          type="checkbox"
+          className="rounded disabled:opacity-50"
+          checked={value.invert}
+          onChange={(e) => onChange({ ...value, invert: e.target.checked })}
+          disabled={isDisabled}
+        />
+      </label>
+      <div className="flex items-center justify-between gap-2 pl-3 text-sm text-neutral-500">
+        <span className="py-2">Smoothness</span>
+        <select
+          className="h-full rounded-tr-md border-0 bg-transparent py-0 text-right text-sm disabled:opacity-50"
+          value={value.smoothness}
+          onChange={(e) => onChange({ ...value, smoothness: parseInt(e.target.value) as Options['smoothness'] })}
+          disabled={isDisabled}
+        >
+          <option value="0">None</option>
+          <option value="1">Low</option>
+          <option value="2">High</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+type PreviewProps = {
+  grid: types.Cell[][];
+  isGenerating: boolean;
+};
+
+function Preview({ grid, isGenerating }: PreviewProps) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!grid.length) return;
+
+    const canvas = canvasRef.current!;
+    const ctx = canvas.getContext('2d')!;
+
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const cellSize = canvas.height / Math.max(rows, cols);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // draw filled
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        if (grid[i][j][0] === 1) {
+          const x = j * cellSize;
+          const y = i * cellSize;
+          ctx.fillStyle = 'black';
+          ctx.fillRect(x, y, cellSize, cellSize);
+        }
+      }
+    }
+
+    // draw the 5x5 groups
+    for (let groupRow = 0; groupRow < Math.ceil(rows / 5); groupRow++) {
+      for (let groupCol = 0; groupCol < Math.ceil(cols / 5); groupCol++) {
+        const groupX = groupCol * 5 * cellSize;
+        const groupY = groupRow * 5 * cellSize;
+        const groupWidth = Math.min(5, cols - groupCol * 5) * cellSize;
+        const groupHeight = Math.min(5, rows - groupRow * 5) * cellSize;
+
+        // group border
+        ctx.strokeStyle = '#737373';
+        ctx.lineWidth = 2;
+        ctx.strokeRect(groupX, groupY, groupWidth, groupHeight);
+
+        for (let i = 0; i < 5; i++) {
+          for (let j = 0; j < 5; j++) {
+            const cellRow = groupRow * 5 + i;
+            const cellCol = groupCol * 5 + j;
+
+            if (cellRow < rows && cellCol < cols) {
+              const x = cellCol * cellSize;
+              const y = cellRow * cellSize;
+
+              // cell border
+              ctx.strokeStyle = '#737373';
+              ctx.lineWidth = 1;
+              ctx.strokeRect(x, y, cellSize, cellSize);
+            }
+          }
+        }
+      }
+    }
+
+    // outer border
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+
+    // adjust the coordinates to account for the 2px border
+    ctx.strokeRect(1, 1, cols * cellSize - 2, rows * cellSize - 2);
+  }, [grid]);
+
+  return (
+    <div className="relative aspect-square">
+      {isGenerating && (
+        <div className="absolute top-2 left-1/2 z-10 -translate-x-1/2 rounded-md border border-yellow-200 bg-yellow-100 px-3 py-1 text-sm text-yellow-900">
+          Generating...
+        </div>
+      )}
+      <div className="absolute inset-0 flex items-center justify-center text-sm text-neutral-500">
+        The generated grid will be shown here
+      </div>
+      <canvas ref={canvasRef} className="relative size-full dark:invert" />
+    </div>
   );
 }
